4bbc7c320c42430351722a87c21106fa453cda1f
Guillaume AGNIERAY <dev@agnieray.net>
2026-01-23 13:06:33 +0100

    Update linting tools

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,7 @@
 module.exports = {
-  "src/**/*.js": files => [
-    `eslint "${files.join('" "')}"`,
-    'grunt js',
-    'git add javascripts/'
-  ],
   "src/**/*.scss": files => [
     `stylelint "${files.join('" "')}"`,
     'grunt css',
-    'git add stylesheets/'
+    'git add stylesheets/ plugins/'
   ],
 }
